568d4847828df42d90b3629607025237b5172791
Ken <71462828+Worldofgentleken@users.noreply.github.com>
2024-04-21 17:54:42 +0900

    Update practice.py

--- zerobase/practice.py
+++ zerobase/practice.py
@@ -155,3 +155,159 @@ print(f'성인 할인대상 {adultDisc}명 요금 : {int(atp[1]):,}원')
 print('='*40)
 print(f'Total : {totalCustomer}명')
 print(f'TotalPrice : {int(totalPrice):,}원')
+
+# 다음과 같이 출력될 수 있도록 재귀함수를 이용해서 팩토리얼 함수를 만들어보자
+# 출력
+# input number : 5
+# 120
+
+def myself(n1):
+    if n1 == 1:
+        return 1
+    else:
+        return n1*myself(n1-1)
+
+n = int(input('input number : '))
+myself(n)
+
+
+# 다음과 같이 출력 될 수 있도록 단리/월복리 게산기 함수를 만들어보자.
+# 입력
+# 예치금(원) : 10000000
+# 기간(년) : 3
+# 연 이율(%) : 3
+# [단리 계산기]
+# ==============================
+# 예치금 : 10,000,000원
+# 예치기간 : 3년
+# 연 이율 : 3%
+# ------------------------------
+# 3년 후 총 수령액 : 10,900,000원
+# ==============================
+
+# [월복리 계산기]
+# ==============================
+# 예치금 : 10,000,000원
+# 예치기간 : 3년
+# 연 이율 : 3%
+# ------------------------------
+# 3년 후 총 수령액 : 10,940,514원
+# ==============================
+
+# 함수
+def interestCalculator(b, y, r):
+    simpleInterest = b + b*r/100*3
+    monthly_rate = r / 100 / 12
+    compoundInterst = b * (1 + monthly_rate) ** (12*y)
+    return [simpleInterest, compoundInterst]
+
+# 입력
+base = int(input('예치금(원): '))
+year = int(input('기간(년): '))
+rate = int(input('연 이율(%): '))
+
+# 실행
+ic = interestCalculator(base, year, rate)
+
+# 출력
+print('[단리 계산기]')
+print('='*30)
+print(f'예치금 : {base:,}원')
+print(f'예치기간 : {year}년')
+print(f'연 이율 : {rate}%')
+print('-'*30)
+print(f'3년 후 총 수령액 : {int(ic[0]):,}원')
+print('='*30)
+print('')
+print('[월복리 계산기]')
+print('='*30)
+print(f'예치금 : {base:,}원')
+print(f'예치기간 : {year}년')
+print(f'연 이율 : {rate}%')
+print('-'*30)
+print(f'3년 후 총 수령액 : {int(ic[1]):,}원')
+print('='*30)
+
+
+# 다음과 같이 출력 될 수 있도록 등차 수열의 n번쨰 값과 합을 출력하는 함수를 만들어 보자.
+# a1 입력 : 2
+# 공차 입력 : 3
+# n 입력 : 7
+# 1번째 항의 값 : 2
+# 1번쨰 항까지의 합 : 2
+# 2번째 항의 값 : 5
+# 2번째 항까지의 합 : 7
+# 3번째 항의 값 : 8
+# 3번째 항까지의 합 : 15
+# 4번째 항의 값 : 11
+# 4번째 항까지의 합 : 26
+# 5번째 항의 값 : 14
+# 5번째 항까지의 합 : 40
+# 6번째 항의 값 : 17
+# 6번째 항까지의 합 : 57
+# 7번째 항의 값 : 20
+# 7번째 항까지의 합 : 77
+
+# 함수
+def numList(n1, n2, n3):
+    n1_List = []
+    for i in range(1, n3 + 1):
+        i = n1 + (i - 1)*n2
+        n1_List.append(i)
+    
+    for j in range(n3):
+        print(f'{j+1}번째 항의 값 : {n1_List[j]}')
+        print(f'{j+1}번째 항까지의 합 : {sum(n1_List[:j+1])}')
+
+# 입력
+a1 = int(input('a1 입력 : '))
+b1 = int(input('공차 입력 : '))
+n = int(input('n 입력 : '))
+
+# 실행
+result = numList(a1, b1, n)
+result
+
+
+# 다음과 같이 출력 될 수 있도록 등비 수열의 n번째 값과 합을 출력하는 함수를 만들어 보자.
+# a1 입력 : 2
+# 공비 입력 : 3
+# n 입력 : 5
+# 1번째 항의 값 : 2
+# 1번째 항까지의 합 : 2
+# 2번째 항의 값 : 6
+# 2번째 항까지의 합 : 8
+# 3번째 항의 값 : 18
+# 3번째 항까지의 합 : 26
+# 4번째 항의 값 : 54
+# 4번째 항까지의 합 : 80
+# 5번째 항의 값 : 162
+# 5번째 항까지의 합 : 242
+
+# 함수
+def sequenceCel(a, b, c):
+    i = 1
+    sumN = 0
+    while i <= c:
+        if i == 1:
+            valueN = a
+            sumN += valueN
+            print(f'{i}번째 항의 값 : {valueN}')
+            print(f'{i}번째 항까지의 합 : {sumN}')
+    
+            i += 1
+            continue
+    
+        valueN *= b
+        sumN += valueN
+        print(f'{i}번째 항의 값 : {valueN}')
+        print(f'{i}번째 항까지의 합 : {sumN}')
+        i += 1
+
+# 입력
+a1 = int(input('a1 입력 : '))
+b1 = int(input('공비 입력 : '))
+n = int(input('n 입력 : '))
+
+# 실행
+sequenceCel(a1,b1,n)
